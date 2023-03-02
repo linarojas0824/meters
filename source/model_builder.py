7a149faf3f3836cf5ead6f5d2abe539546add573
Lina Rojas <lina.rojas0824@gmail.com>
2023-03-02 17:21:25 -0600

    model_builder_update

--- source/model_builder.py
+++ source/model_builder.py
@@ -1,5 +1,6 @@
 
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
 import numpy as np
 from sklearn.metrics import accuracy_score
 
@@ -32,3 +33,28 @@ class ModelBuilder(DataPreprocessing):
         self.accuracy = accuracy_score(y_test, DT_predicted)
 
         return DT_classifier
+
+class ModelANN(DataPreprocessing):
+    def __init__(self, *args, **kwargs):
+        super(ModelANN, self).__init__(*args, **kwargs)
+
+    def dt(self, X_train, X_test, y_train, y_test):
+        #Create DT model
+        MANN_classifier = MLPClassifier()
+
+        #Train the model
+        MANN_classifier.fit(X_train, y_train)
+
+        #Test the model
+        DT_predicted = MANN_classifier.predict(X_test)
+
+        error = 0
+        for i in range(len(y_test)):
+            error = np.sum(DT_predicted != y_test)
+
+        total_accuracy = 1 - error / len(y_test)
+
+        #get performance
+        self.accuracy = accuracy_score(y_test, DT_predicted)
+
+        return MANN_classifier
