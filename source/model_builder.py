8a2afbfd3df9a9c9db26af0c40c483138a65d7b0
Lina Rojas <lina.rojas0824@gmail.com>
2023-03-02 18:41:39 -0600

    fina_update

--- source/model_builder.py
+++ source/model_builder.py
@@ -34,27 +34,3 @@ class ModelBuilder(DataPreprocessing):
 
         return DT_classifier
 
-class ModelANN(DataPreprocessing):
-    def __init__(self, *args, **kwargs):
-        super(ModelANN, self).__init__(*args, **kwargs)
-
-    def dt(self, X_train, X_test, y_train, y_test):
-        #Create DT model
-        MANN_classifier = MLPClassifier()
-
-        #Train the model
-        MANN_classifier.fit(X_train, y_train)
-
-        #Test the model
-        DT_predicted = MANN_classifier.predict(X_test)
-
-        error = 0
-        for i in range(len(y_test)):
-            error = np.sum(DT_predicted != y_test)
-
-        total_accuracy = 1 - error / len(y_test)
-
-        #get performance
-        self.accuracy = accuracy_score(y_test, DT_predicted)
-
-        return MANN_classifier
